15af927bee60f6be7a0d649cb734765b2c506550
MatovicB <117674984+MatovicB@users.noreply.github.com>
2023-12-07 17:23:05 +0100

    test test

--- js/mojJS.js/main.js
+++ js/mojJS.js/main.js
@@ -1,7 +1,7 @@
 
 
 var nizNavigacioniMeniHref = ["index.html","about.html","games.html","tranner.html","contact.html"];
-var nizNavigacioniMeniTekst = ["Pocetna","O nama","Utakmice","Treneri","Kontakt"];
+var nizNavigacioniMeniTekst = ["Pocetna2","O nama","Utakmice","Treneri","Kontakt"];
 
 var navigacioniMeniHTML = document.querySelector("#mySidepanel");
 
